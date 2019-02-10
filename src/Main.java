6439b06cdadc401b6d21a1d1f3be22fd40b50673
syed <shamulh@gmail.com>
2019-02-09 20:04:12 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.println("Tryihg hard to get over github");
+        System.out.println("Trying to get my hand on Github");
+        System.out.println("Hello there I am getting some new stuff");
+    }
+}
